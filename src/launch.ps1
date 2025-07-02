856bcf8dab091926e631d7fb8f0646c3f37b6eda
Spencer Frisby <frisbysg@gmail.com>
2025-07-02 10:16:24 -0600

    methods for episode handling

--- src/launch.ps1
+++ src/launch.ps1
@@ -32,6 +32,7 @@ Playing (then exit) the file via VLC at 1.5 times the normal playback speed:
 
 #>
 $cmd = {
+
     $Host.UI.RawUI.WindowTitle = 'TEST pwsh podcasts';
     New-Variable -Name configuration (.\src\setup.ps1);
     New-Variable -Name stopwatch ([Diagnostics.Stopwatch]::StartNew())
@@ -39,5 +40,43 @@ $cmd = {
     $stopwatch.Stop()
     . .\src\utilities.ps1;
     # .\test\show_podcasts.ps1 $podcasts
+
+    function Show-Recent-Episodes {
+        param (
+            [parameter(Position = 0)]
+            [int] $Count = 10
+        )
+        $script:index = 0;
+        $podcasts | Select-Object -Property @{n = 'index'; e = { ($script:index++) } }, @{n = 'date'; e = { [datetime] $_.date } }, title, podcast | Sort-Object -Property date -Descending | Select-Object -First $Count
+    }
+
+    function Start-Episode-Stream {
+        param (
+            [parameter(Mandatory, Position = 0)]
+            [int] $Episode,
+            [parameter(Position = 1)]
+            [Single] $Rate = 1.5
+        )
+        & $configuration[1].vlc $podcasts[$Episode].url --rate $Rate --play-and-exit
+    }
+
+    function Start-Episode-File {
+        param (
+            [parameter(Mandatory, Position = 0)]
+            [string] $File,
+            [parameter(Position = 1)]
+            [Single] $Rate = 1.5
+        )
+        & $configuration[1].vlc $File --rate $Rate --play-and-exit
+    }
+
+    function Start-Episode-Download {
+        param (
+            [parameter(Mandatory, Position = 0)]
+            [int] $Episode
+        )
+        .\test\invoke_brave_download.ps1 $podcasts[$Episode]
+    }
 }
+
 Start-Process pwsh -WorkingDirectory $(Join-Path $PSScriptRoot ".." ".") -WindowStyle Normal -ArgumentList "-NoExit", "-Command `"$($cmd)`"";
